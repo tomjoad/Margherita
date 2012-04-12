ab326f0a9e6d70ac17a6dcc8c4bc3c9d1faea8d8
lukasz <lukasz@lukasz-ThinkPad-T61.(none)>
2012-04-12 20:30:27 +0200

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Margherita::Application.config.secret_token = 'd31172f7f8138396454b1b3de9d5b918a1a46eabfeadcf3c48e08e98cc4da4182f1ba3676fd85a40c09eb909bfae4e6be5d9432b4a35eb3ee85bda251c9f27a3'
