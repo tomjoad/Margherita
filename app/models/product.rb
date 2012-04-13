47ce8e5b35aad3397e0b7ea1f3491f4e9cbf9bbe
lukasz <lukasz@lukasz-ThinkPad-T61.(none)>
2012-04-13 22:03:59 +0200

    test functions

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,10 @@
 class Product < ActiveRecord::Base
 
+  validates :name, :presence => true
+  validates :price, :presence => true
+
   def price_in_dolars
-    return "#{@price} $"
+   read_attribute(:price) + 12.0
   end
 
 end
