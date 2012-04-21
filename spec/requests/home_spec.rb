2478ae44110ebca03d44f21c9ac3b482bcc6cfaa
lukasz <lukasz@lukasz-ThinkPad-T61.(none)>
2012-04-21 22:13:54 +0200

    added some rspec tests for views

---
+++ spec/requests/home_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe "Home" do
+
+  let(:base_title) { "Margherita" }
+
+  describe "Home Page" do
+    it "should have the h1 'Margherita'" do
+      visit '/'
+      page.should have_selector('h1', :text => "#{base_title}")
+    end
+    it "should have the title 'Margherita'" do
+      visit '/'
+      page.should have_selector('title', :text => "#{base_title} | Home")
+    end
+  end
+
+  describe "Help page" do
+    it "should have h1 'Help'" do
+      visit '/home/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+    it "should have title 'Help'" do
+      visit '/home/help'
+      page.should have_selector('title', :text => "#{base_title} | Help")
+    end
+  end
+
+  describe "About page" do
+    it "should have content 'About'" do
+      visit '/home/about'
+      page.should have_selector('h1', :text => 'About')
+    end
+    it "should have title 'About'" do
+      visit '/home/about'
+      page.should have_selector('title', :text => "#{base_title} | About")
+    end
+  end
+
+end
