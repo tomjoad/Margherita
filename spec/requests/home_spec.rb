b85de8043c6338d1d9ecf7e0f2d22527f726dcca
lukasz <lukasz@lukasz-ThinkPad-T61.(none)>
2012-04-27 09:51:05 +0200

    even more fancy tests

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -5,22 +5,32 @@ describe "Home" do
   let(:base_title) { "Margherita" }
   subject { page }
 
+  shared_examples_for "all home pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
+
   describe "Home Page" do
     before { visit root_path }
-    it { should have_selector('h1', :text => "#{base_title}") }
-    it { should have_selector('title', :text => full_title("Home")) }
+    let(:heading) { 'Margherita' }
+    let(:page_title) { "" }
+    it_should_behave_like "all home pages"
+
+    it { should_not have_selector "title", text: "| Home" }
   end
 
   describe "Help page" do
     before { visit home_help_path }
-    it { should have_selector('h1', :text => 'Help') }
-    it { should have_selector('title', :text => full_title("Help")) }
+    let(:heading) { 'Help' }
+    let(:page_title) { 'Help' }
+    it_should_behave_like "all home pages"
   end
 
   describe "About page" do
     before { visit home_about_path }
-    it { should have_selector('h1', :text => 'About') }
-    it { should have_selector('title', :text => full_title("About")) }
+    let(:heading) { 'About' }
+    let(:page_title) { 'About' }
+    it_should_behave_like "all home pages"
   end
 
 end
