d1cb2cec1313a297b532fe686d58f3849e603f58
n0stromo <barfek@gmail.com>
2012-10-17 21:49:53 +0200

    Fixing Heroku issues v4

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-config.assets.initialize_on_precompile = false
+
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -56,5 +56,6 @@ module Margherita
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.assets.initialize_on_precompile = false
   end
 end
