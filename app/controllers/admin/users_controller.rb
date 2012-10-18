c61d4b8a0737ba6ef6c4d8f1f9515fb12f0ee039
lukasz <lukasz@lukasz-ThinkPad-T61.(none)>
2012-10-18 14:11:24 +0200

    cleaning

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -15,17 +15,34 @@ class Admin::UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  # Strange way to upgrade only one attribute.
+  # Writing straight to db, overriding validations
+  # and mass-assignment security.
+  # Only for updating user roles.
+
   def update
     @user = User.find(params[:id])
-    if User.update_all(params[:user], {:id => @user.id})
-      flash[:notice] = "updated"
+    unless changing_his_own_role?(@user)
+      User.update_all(params[:user], {:id => @user.id})
+      #  if @user.update_attribute(:role, params[:user][:role])
+      # if @user.send(:attributes=, {:role => params[:user][:role]})
+      flash[:notice] = "User role updated!"
       redirect_to admin_users_path
+      # else
+      #   flash[:notice] = "didn`t work"
+      #   redirect_to admin_users_path
+      # end
     else
-      flash[:notice] = "didn`t work"
+      flash[:notice] = "Can`t overwrite your own rights!"
       redirect_to admin_users_path
     end
   end
-  # private
+
+  private
+
+  def changing_his_own_role?(user)
+    current_user.id == user.id
+  end
 
   # def user_should_be_admin
   #   unless current_user.role.admin?
