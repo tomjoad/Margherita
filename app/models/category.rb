9fed19269573751c21c61fc5af8ccd794a1dcc54
n0stromo <barfek@gmail.com>
2013-02-10 12:18:59 +0100

    Put category fixed array into model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,12 @@
+# -*- coding: utf-8 -*-
 class Category < ActiveRecord::Base
   has_many :variants, :through => :products
   has_many :products
 
   validates :name, :presence => true
+
+  def self.manual_order
+    [ 'Pizza', 'Dania Obiadowe', 'Spaghetti', 'Naleśniki', 'Fast Food', 'Sałatki', 'Zupy', 'Sosy', 'Dodatki' ]
+  end
 
 end
