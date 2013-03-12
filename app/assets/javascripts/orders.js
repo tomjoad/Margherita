0c39a7e671c2c7eacabba9c397a4ecc87f211f9a
n0stromo <barfek@gmail.com>
2013-03-12 20:59:09 +0100

    Working with order.js & views

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -13,23 +13,25 @@ var bounds = new google.maps.LatLngBounds(
 );
 var marker = new google.maps.Marker();
 var origin = new google.maps.LatLng(50.186428, 18.801008);
-var toAddressLanLng
+var toAddressLanLng;
+
 
 window.onload = initialize();
-document.getElementById("btn").onclick = function(){codeAddress()};
 
 function initialize() {
+    document.getElementById("btn").onclick = function(){codeAddress()};
     geocoder = new google.maps.Geocoder();
     var mapOptions = {
-        zoom: 18,
+        zoom: 19,
         center: origin,
         mapTypeId: google.maps.MapTypeId.HYBRID
     }
     map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-    map.fitBounds(bounds);
+    // map.fitBounds(bounds);
     directionsDisplay.setMap(map);
 
-    codeAddress();
+
+
 }
 
 function calcRoute() {
@@ -49,13 +51,13 @@ function calcRoute() {
 }
 
 function computeTotalDistance(result) {
-  var total = 0;
-  var myroute = result.routes[0];
-  for (i = 0; i < myroute.legs.length; i++) {
-    total += myroute.legs[i].distance.value;
-  }
-  total = total / 1000.
-  document.getElementById("order_accurate_distance").value = total + " km";
+    var total = 0;
+    var myroute = result.routes[0];
+    for (i = 0; i < myroute.legs.length; i++) {
+        total += myroute.legs[i].distance.value;
+    }
+    total = total / 1000.
+        document.getElementById("order_accurate_distance").value = total + " km";
 }
 
 function codeAddress() {
@@ -67,24 +69,15 @@ function codeAddress() {
         'address': address,
         'bounds': bounds // doesnt work. google bug.
     }, function(results, status) {
+        // document.getElementById("geocoder_result").innerHTML = results[0].formatted_address;
         if (status == google.maps.GeocoderStatus.OK) {
-            var i = 0;
-            var end = results.length;
-            console.log(results);
-            while (results[i].formatted_address.indexOf(city) == -1
-                   && results[i].formatted_address.indexOf(street) == -1
-                   && i<results.length-1)
-            {
-                i++;
-                console.log(i);
-            }
             marker.setMap(null);
-            map.setCenter(results[i].geometry.location);
+            map.setCenter(results[0].geometry.location);
             map.setZoom(17);
             marker = new google.maps.Marker({
                 draggable: true,
                 map: map,
-                position: results[i].geometry.location,
+                position: results[0].geometry.location,
                 title: "dokładne miejsce dostawy"
             });
             getMarkerLocation();
